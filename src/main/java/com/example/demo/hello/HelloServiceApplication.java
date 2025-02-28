32b266f3460ec76d59de67126e3ca5647ad39dca
keerthi-sri-0625 <kanakalakeerthisri@gmail.com>
2025-02-28 17:38:53 +0530

    Initial commit with Java project and pom.xml

---
+++ src/main/java/com/example/demo/hello/HelloServiceApplication.java
@@ -0,0 +1,28 @@
+package com.example.demo.hello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@EnableEurekaClient 
+public class HelloServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelloServiceApplication.class, args);
+    }
+}
+
+@RestController
+class HelloController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello from HelloService!";
+    }
+}
+
+
+
